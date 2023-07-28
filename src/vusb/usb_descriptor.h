bf5b2bd2dc64322d63ded14f1a9bef3eede27d26
Todd Herbert <herbert.todd@gmail.com>
2023-07-29 01:24:26 +1200

    chore: tidy HID Report

--- src/vusb/usb_descriptor.h
+++ src/vusb/usb_descriptor.h
@@ -52,30 +52,8 @@ PROGMEM const uint8_t usbHidReportDescriptor[] =  {
     0x81, 0x06,                    //     INPUT (Data,Var,Rel)
     0xC0,                          //       END_COLLECTION
     0xC0,                          // END COLLECTION
-
-    // Keyboard report to go here
-
-    // 0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-    // 0x09, 0x06,                    // USAGE (Keyboard)
-    // 0xa1, 0x01,                    // COLLECTION (Application)
-    // 0x85, 0x02,                    //  Report ID 2 
-    // 0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
-    // 0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)
-    // 0x29, 0xe7,                    //   USAGE_MAXIMUM (Keyboard Right GUI)
-    // 0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    // 0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
-    // 0x75, 0x01,                    //   REPORT_SIZE (1)
-    // 0x95, 0x08,                    //   REPORT_COUNT (8)
-    // 0x81, 0x02,                    //   INPUT (Data,Var,Abs)
-    // 0x95, 0x01,                    //   REPORT_COUNT (1)
-    // 0x75, 0x08,                    //   REPORT_SIZE (8)
-    // 0x25, 0x65,                    //   LOGICAL_MAXIMUM (101)
-    // 0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))
-    // 0x29, 0x65,                    //   USAGE_MAXIMUM (Keyboard Application)
-    // 0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
-    // 0xc0                           // END_COLLECTION
-
- //  Arduino default keyboard library
+ 
+    // Keyboard report, from Arduino official keyboard library
     0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)  // 47
     0x09, 0x06,                    // USAGE (Keyboard)
     0xa1, 0x01,                    // COLLECTION (Application)
